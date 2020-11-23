0ffb219a06cda713c7523b050f0b6e6ae21a59f9
Sharvani2002 <sharvanilaxmisomayaji@gmail.com>
2020-11-23 14:35:39 +0530

    Update1

--- Task2/pkg_task2/scripts/node_t2_ur5_1_pick_place.py
+++ Task2/pkg_task2/scripts/node_t2_ur5_1_pick_place.py
@@ -91,7 +91,7 @@ class MoveGroupPythonIntefaceTutorial(object):
     ##
     ## First initialize `moveit_commander`_ and a `rospy`_ node:
     moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_it_tutorial', anonymous=True)
+    rospy.init_node('node_t2_ur5_1_pick_place', anonymous=True)
 
     ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
     ## kinematic model and the robot's current joint states
@@ -123,6 +123,10 @@ class MoveGroupPythonIntefaceTutorial(object):
 
     # We can also print the name of the end-effector link for this group:
     eef_link = move_group.get_end_effector_link()
+    # See the /home/somayaji/catkin_ws/src/vb_simulation_pkgs/pkg_vb_sim/config/config_vacuum_gripper.yaml
+    # for more details on vacuum gripper
+    # vacuum_gripper_model_name: "ur5_1"
+    # vacuum_gripper_link_name: "vacuum_gripper_link"
     eef_link = "vacuum_gripper_link"
     print("============ End effector link: %s" % eef_link)
 
@@ -137,7 +141,7 @@ class MoveGroupPythonIntefaceTutorial(object):
     print("")
 
     # Misc variables
-    self.box_name = ''
+    self.box_name = 'package$1'
     self.robot = robot
     self.scene = scene
     self.move_group = move_group
@@ -341,128 +345,133 @@ class MoveGroupPythonIntefaceTutorial(object):
   #   ## END_SUB_TUTORIAL
 
 
-    def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
+  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
 
-        ## BEGIN_SUB_TUTORIAL wait_for_scene_update
-        ## Ensuring Collision Updates Are Received
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## If the Python node dies before publishing a collision object update message, the message
-        ## could get lost and the box will not appear. To ensure that the updates are
-        ## made, we wait until we see the changes reflected in the
-        ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
-        ## For the purpose of this tutorial, we call this function after adding,
-        ## removing, attaching or detaching an object in the planning scene. We then wait
-        ## until the updates have been made or ``timeout`` seconds have passed
-        start = rospy.get_time()
-        seconds = rospy.get_time()
-        while (seconds - start < timeout) and not rospy.is_shutdown():
-          # Test if the box is in attached objects
-          attached_objects = scene.get_attached_objects([box_name])
-          is_attached = len(attached_objects.keys()) > 0
+    box_name = self.box_name
+    scene = self.scene
 
-          # Test if the box is in the scene.
-          # Note that attaching the box will remove it from known_objects
-          is_known = box_name in scene.get_known_object_names()
+    ## BEGIN_SUB_TUTORIAL wait_for_scene_update
+    ## Ensuring Collision Updates Are Received
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ## If the Python node dies before publishing a collision object update message, the message
+    ## could get lost and the box will not appear. To ensure that the updates are
+    ## made, we wait until we see the changes reflected in the
+    ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
+    ## For the purpose of this tutorial, we call this function after adding,
+    ## removing, attaching or detaching an object in the planning scene. We then wait
+    ## until the updates have been made or ``timeout`` seconds have passed
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+      # Test if the box is in attached objects
+      attached_objects = scene.get_attached_objects([box_name])
+      is_attached = len(attached_objects.keys()) > 0
 
-          # Test if we are in the expected state
-          if (box_is_attached == is_attached) and (box_is_known == is_known):
-            return True
+      # Test if the box is in the scene.
+      # Note that attaching the box will remove it from known_objects
+      is_known = box_name in scene.get_known_object_names()
 
-          # Sleep so that we give other threads time on the processor
-          rospy.sleep(0.1)
-          seconds = rospy.get_time()
+      # Test if we are in the expected state
+      if (box_is_attached == is_attached) and (box_is_known == is_known):
+        return True
 
-        # If we exited the while loop without returning then we timed out
-        return False
-        ## END_SUB_TUTORIAL
+      # Sleep so that we give other threads time on the processor
+      rospy.sleep(0.1)
+      seconds = rospy.get_time()
 
-
-    def add_box(self, timeout=6):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
+    # If we exited the while loop without returning then we timed out
+    return False
+    ## END_SUB_TUTORIAL
 
 
-        ## Adding Objects to the Planning Scene
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## First, we will create a box in the planning scene between the fingers:
-        box_pose = geometry_msgs.msg.PoseStamped()
-        
-        # box_pose.header.frame_id = "panda_hand"
-        box_pose.header.frame_id = "/base_link"
+  def add_box(self, timeout=6):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    self.box_name = 'package$1'
+    box_name = self.box_name
+    scene = self.scene
 
-        box_pose.pose.orientation.w = 0.0
-        box_pose.pose.orientation.x = 0.0
-        box_pose.pose.orientation.y = 0.0
-        box_pose.pose.orientation.z = 0.0
-        box_pose.pose.position.x = 0.036260 
-        box_pose.pose.position.y = 0.306889 
-        box_pose.pose.position.z = 1.965703
-        box_name = "box"
-        scene.add_box(box_name, box_pose, size=(0.15, 0.15, 0.15))
 
-        # Copy local variables back to class variables. In practice, you should use the class
-        # variables directly unless you have a good reason not to.
-        self.box_name=box_name
-        return self.wait_for_state_update(box_is_known=True, timeout=timeout)
+    ## Adding Objects to the Planning Scene
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ## First, we will create a box in the planning scene between the fingers:
+    box_pose = geometry_msgs.msg.PoseStamped()
+    
+    # box_pose.header.frame_id = "panda_hand"
+    # box_pose.header.frame_id = "/base_link"
+    box_pose.header.frame_id = "world"
 
-    def attach_box(self, timeout=6):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        robot = self.robot
-        scene = self.scene
-        eef_link = self.eef_link
-        group_names = self.group_names
+    box_pose.pose.orientation.w = 0.0
+    box_pose.pose.orientation.x = 0.0
+    box_pose.pose.orientation.y = 0.0
+    box_pose.pose.orientation.z = 0.0
+    box_pose.pose.position.x = 0.036260 
+    box_pose.pose.position.y = 0.306889 
+    box_pose.pose.position.z = 1.965703
+    # box_name = "box"
+    box_name = 'package$1'
+    scene.add_box(box_name, box_pose, size=(0.15, 0.15, 0.15))
 
-        ## Attaching Objects to the Robot
+    # Copy local variables back to class variables. In practice, you should use the class
+    # variables directly unless you have a good reason not to.
+    self.box_name=box_name
+    return self.wait_for_state_update(box_is_known=True, timeout=timeout)
 
-        ## Next, we will attach the box to the Panda wrist. Manipulating objects requires the
-        ## robot be able to touch them without the planning scene reporting the contact as a
-        ## collision. By adding link names to the ``touch_links`` array, we are telling the
-        ## planning scene to ignore collisions between those links and the box. For the Panda
-        ## robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
-        ## you should change this value to the name of your end effector group name.
-        grasping_group = 'hand'
-        touch_links = robot.get_link_names(group=grasping_group)
-        scene.attach_box(eef_link, box_name, touch_links=touch_links)
+  def attach_box(self, timeout=6):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    box_name = self.box_name
+    robot = self.robot
+    scene = self.scene
+    eef_link = self.eef_link
+    group_names = self.group_names
 
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
+    ## Attaching Objects to the Robot
 
-    def detach_box(self, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
-        eef_link = self.eef_link
+    ## Next, we will attach the box to the Panda wrist. Manipulating objects requires the
+    ## robot be able to touch them without the planning scene reporting the contact as a
+    ## collision. By adding link names to the ``touch_links`` array, we are telling the
+    ## planning scene to ignore collisions between those links and the box. For the Panda
+    ## robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
+    ## you should change this value to the name of your end effector group name.
+    # grasping_group = 'hand'
+    grasping_group = "ur5_1_planning_group"
+    touch_links = robot.get_link_names(group=grasping_group)
+    scene.attach_box(eef_link, box_name, touch_links=touch_links)
 
-        ## Detaching Objects from the Robot
+    # We wait for the planning scene to update.
+    return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
 
-        ## We can also detach and remove the object from the planning scene:
-        scene.remove_attached_object(eef_link, name=box_name)
+  def detach_box(self, timeout=4):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    box_name = self.box_name
+    scene = self.scene
+    eef_link = self.eef_link
 
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
+    ## Detaching Objects from the Robot
 
-    def remove_box(self, timeout=4):
-        box_name = self.box_name
-        scene = self.scene
+    ## We can also detach and remove the object from the planning scene:
+    scene.remove_attached_object(eef_link, name=box_name)
 
-        scene.remove_world_object(box_name)
+    # We wait for the planning scene to update.
+    return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
 
-        ## **Note:** The object must be detached before we can remove it from the world
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(box_is_attached=False, box_is_known=False, timeout=timeout)
+  def remove_box(self, timeout=4):
+    box_name = self.box_name
+    scene = self.scene
+
+    scene.remove_world_object(box_name)
+
+    ## **Note:** The object must be detached before we can remove it from the world
+    # We wait for the planning scene to update.
+    return self.wait_for_state_update(box_is_attached=False, box_is_known=False, timeout=timeout)
 
 
 def main():
@@ -477,15 +486,22 @@ def main():
     rospy.sleep(4)
     tutorial.add_box()
     rospy.sleep(6)
+    tutorial.attach_box()
+    rospy.sleep(6)
     #execute a movement using a pose goal
     tutorial.go_to_pose_goal2()
     rospy.sleep(4)
     gripper_on()
     rospy.sleep(2)
+
     # tutorial.attach_box()
     #execute a movement using a pose goal
     tutorial.go_to_pose_goal3()
     rospy.sleep(4)
+    tutorial.detach_box()
+    tutorial.remove_box()
+    rospy.sleep(6)
+
     # tutorial.detach_box()
     gripper_off()
     rospy.sleep(2)
